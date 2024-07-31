0a879f501fdf6ecfc0eac287d3370dd69e7532f6
Laxmi Kumari <165251933+laxmibcci@users.noreply.github.com>
2024-07-31 18:27:55 +0530

    operator & conditionals

---
+++ operatorcond.js
@@ -0,0 +1,143 @@
+// console.log("laxmi")
+
+
+//** Arithmetic operator **//
+// let a = 2;
+// let b = 5;
+
+// console.log(a+b);
+
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+
+// console.log(a%b);
+
+// console.log(a**b);
+
+let a = 2;
+let b = 5;
+ 
+
+//** Assignment & comparison operator  **//
+
+
+// a = a +10;
+// a +=10;
+// console.log(a);
+
+// a = a -2;
+// a -=2;
+// console.log(a);
+
+// console.log(5>=5)
+
+// console.log('5' == 5);
+// console.log('5' === 5);
+// console.log(5 === 5);
+
+
+// console.log('5' != 3);
+
+// console.log('5' !== 3);
+// console.log(5 !== '3');
+ 
+
+
+
+//**Ternary operator **//
+
+// let age = 25;
+// let status1 = (age > 18) ? 'I can vote' : 'I cannot vote';
+
+// console.log(status1);
+
+
+
+
+//** Logical operator **//
+
+// let ans = (true && true && true);
+// console.log(ans);
+
+// let ans = (true && true && false);
+// console.log(ans);
+
+
+// let ans = (true || false || false);
+// console.log(ans);
+
+// let ans = (false ||false|| false);
+// console.log(ans);
+
+// let ans = !(true);
+// console.log(ans)
+
+// let ans = !(false);
+// console.log(ans);
+
+// console.log(false||'babbar');
+// console.log(false||7||11||18);
+
+
+
+//** Bitwise operator **//
+// console.log(0&1);
+// console.log(1 & 1);
+// console.log(1|0);
+// console.log(~1);
+// console.log(2^2);
+
+
+
+
+//** conditional **//
+// let age = 30;
+// if (age >=18) {
+//     console.log('can vote');
+// }
+// else{
+//     console.log('cannot vote');
+// }
+
+// let number = 5;
+// if (number ==1 ) {
+//     console.log('A');
+// }
+// if (number ==2 ) {
+//     console.log('B');
+// }
+// if (number ==3 ) {
+//     console.log('C');
+// }
+
+// if (number ==4 ) {
+//     console.log('D');
+// }
+
+// if (number ==5 ) {
+//     console.log('E');
+// }
+
+// else{
+//     console.log('F');
+// }
+
+
+//** switch number **//
+
+let num =3;
+switch(num) {
+    case 1 : console.log('A');
+    break;
+    case 2 : console.log('B');
+    break;
+    case 3 : console.log('C');
+    break;
+    case 4 : console.log('D');
+    break;
+    default : console.log('F');
+}
+
