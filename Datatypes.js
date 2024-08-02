3e73de513b1dd409f06d413709f8be2a469cc878
Laxmi Kumari <165251933+laxmibcci@users.noreply.github.com>
2024-08-02 06:30:31 +0000

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,21 @@
+// Number => 2 to power 55
+// bigint => big number
+// String => ""
+// boolean => true/false
+// null => Standalone value
+// undefined => 
+// Symbol => unique
+
+
+// object 
+
+
+
+let name = "laxmi"
+// knowing of datatypes
+console.log(typeof "laxmi"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
