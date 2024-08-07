91eaefe7aa8ec1e1bad27f2000f7f4d9874e8909
Laxmi Kumari <165251933+laxmibcci@users.noreply.github.com>
2024-08-07 06:57:10 +0000

    add file

--- Datatypes.js
+++ Datatypes.js
@@ -11,11 +11,61 @@
 
 
 
-let name = "laxmi"
+// let name = "laxmi"
 // knowing of datatypes
-console.log(typeof "laxmi"); // string
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof "laxmi"); // string
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+  
+
+// new line about datatypes
+
+// Two types of datatypes:-
+// 1. primitive
+// 2. Non-primitive
+
+// 1.explaination of primitive:-
+// 7 type of primitive:-
+// a. String:- const str = "150"
+// b. Number:- const score=100
+// 
+
+// c. Bigint:-
+
+//  const Bignumber = 8842874683748247647637n
+// d. Boolean:-
+// e. null:- const outsidetemp = null
+// f. undefined:- let useremail;
+// g. Symbol:-  
+            //     const id = symbol('123')
+            //    const anotherId = symbol('123')
+            //    console.log(id === anotherId)
+
+// 2. explaination of Reference Non-primitive
+// a. Array
+     const heros = ["shaktiman","naagraj", "doga"];
+     
+
+// b. objects
+      let  myObj = {
+        name: "laxmi",
+        age: 22,
+      }
+    
+
+// c. function
+     const myfunction = function() {
+        console.log("hello world")
+
+     }
 
 
+     //how to which datatypes use
 
+     console.log(typeof  Bignumber);
+     
+
+    // use of typeof for know about which datatypes
+
+
+              
