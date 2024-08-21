d49a6227f60c2740814957279a2765f69ab81190
Laxmi Kumari <165251933+laxmibcci@users.noreply.github.com>
2024-08-21 16:48:05 +0000

    file add

---
+++ Functions.js
@@ -0,0 +1,16 @@
+// Function definition
+// function sayMyName() {
+//     console.log("laxmi Kumari")
+// }
+
+//function use - function call
+// sayMyName();
+
+function printcounting(){
+    for(let i=1; i<=20; i++) {
+        console.log(i);
+        
+    }
+}
+
+printcounting();
