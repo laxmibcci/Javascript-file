fb6a34496880d4845942ae39677c8faaa328e157
Laxmi Kumari <165251933+laxmibcci@users.noreply.github.com>
2024-08-23 16:32:33 +0000

    file add

--- Functions.js
+++ Functions.js
@@ -1,4 +1,5 @@
 // Function definition
+/************** function without parameter */
 // function sayMyName() {
 //     console.log("laxmi Kumari")
 // }
@@ -6,26 +7,63 @@
 //function use - function call
 // sayMyName();
 
-function printcounting(){
-    for(let i=1; i<=20; i++) {
-        console.log(i);
-        
-    }
-}
-
-printcounting();
 
 
-/******************** 3rd (parameters ) */
+/************** function with loop */
+// function printcounting(){
+    // for(let i=1; i<=20; i++) {
+        // console.log(i);
+        // }
+// }
+
+// printcounting();
+
+
+/******************** 3rd ( function with single parameters ) */
 
 // function PrintNumber(num){
     // console.log( "Printing Number:" ,num);
 //   }
-PrintNumber(5);
+// PrintNumber(5);
 /****************** (num) = parameters and (5) = arguments */
 
 
 
 
 
-/****************** 4th (multiparameters) */
+/****************** 4th ( function with multiparameters) */
+
+// function getAverage(num1,num2){
+//     let avg = (num1 + num2)/2
+//     console.log("Average:" ,avg);
+    // }
+// getAverage(3,70)
+
+/********************* Return Function */
+// function getsum(a,b,c) {
+//     let sum = a + b + c;
+//     return sum;
+// }
+// let ans = getsum(1,2,3);
+// console.log(" getsum:", ans);
+
+
+//****** 2nd */
+
+// function getMyName(firstName, lastName){
+// let fullName = firstName +  " "  + lastName;
+// return fullName;
+// }
+// let fullName = getMyName("Laxmi", "Kumari");
+// console.log("fullName: ", fullName);
+
+
+//**************** function with number  */
+function getMyName(firstName, lastName){
+    let fullName = firstName +  " "  + lastName;
+    return fullName;
+    }
+    let fullName = getMyName("Laxmi", "7");
+    console.log("fullName: ", fullName);
+    
+
